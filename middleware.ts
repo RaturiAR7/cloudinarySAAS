79039e3fc8616b27a5e0879008f1f6a375614ab6
Anshul <anshulraturi007@gmail.com>
2024-08-11 23:41:20 +0530

    Configure Daisy UI and Clerk

---
+++ middleware.ts
@@ -0,0 +1,32 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+const isPublicRoute = createRouteMatcher(["/signin", "/signup", "/", "/home"]);
+const isPublicApiRoute = createRouteMatcher(["/api/videos"]);
+
+export default clerkMiddleware((auth, req) => {
+  const { userId } = auth();
+  const currentUrl = new URL(req.url);
+  const isAccessingDashboard = currentUrl.pathname === "/home";
+  const isApiRequest = currentUrl.pathname.startsWith("/api");
+  ////logged in
+  if (userId && isPublicRoute(req) && !isAccessingDashboard) {
+    return NextResponse.redirect(new URL("/home", req.url));
+  }
+  ////Not Logged In
+  if (!userId) {
+    ////If user not logged in and trying to access a protected route
+    if (!isPublicRoute(req) && !isPublicApiRoute(req)) {
+      return NextResponse.redirect(new URL("/signin", req.url));
+    }
+    ////If the request is for a protected API and the user is not logged in
+    if (isApiRequest && !isPublicApiRoute(req)) {
+      return NextResponse.redirect(new URL("/signin", req.url));
+    }
+  }
+  return NextResponse.next();
+});
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*", "/", "/(api|trpc)(.*)"],
+};
