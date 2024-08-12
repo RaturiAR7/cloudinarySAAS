050fb2a0450795fb574e5fd71ac6b9734844fb85
Anshul <anshulraturi007@gmail.com>
2024-08-12 15:24:45 +0530

    Clerk authentication setup complete

---
+++ app/(app)/video-upload/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function VideoUpload() {
+  return (
+    <div>
+      <h1>VideoUpload</h1>
+    </div>
+  );
+}
+
+export default VideoUpload;
--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,12 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-const isPublicRoute = createRouteMatcher(["/signin", "/signup", "/", "/home"]);
+const isPublicRoute = createRouteMatcher([
+  "/sign-in",
+  "/sign-up",
+  "/",
+  "/home",
+]);
 const isPublicApiRoute = createRouteMatcher(["/api/videos"]);
 
 export default clerkMiddleware((auth, req) => {
@@ -9,24 +14,26 @@ export default clerkMiddleware((auth, req) => {
   const currentUrl = new URL(req.url);
   const isAccessingDashboard = currentUrl.pathname === "/home";
   const isApiRequest = currentUrl.pathname.startsWith("/api");
-  ////logged in
+
+  // If user is logged in and accessing a public route but not the dashboard
   if (userId && isPublicRoute(req) && !isAccessingDashboard) {
     return NextResponse.redirect(new URL("/home", req.url));
   }
-  ////Not Logged In
+  //not logged in
   if (!userId) {
-    ////If user not logged in and trying to access a protected route
+    // If user is not logged in and trying to access a protected route
     if (!isPublicRoute(req) && !isPublicApiRoute(req)) {
-      return NextResponse.redirect(new URL("/signin", req.url));
+      return NextResponse.redirect(new URL("/sign-in", req.url));
     }
-    ////If the request is for a protected API and the user is not logged in
+
+    // If the request is for a protected API and the user is not logged in
     if (isApiRequest && !isPublicApiRoute(req)) {
-      return NextResponse.redirect(new URL("/signin", req.url));
+      return NextResponse.redirect(new URL("/sign-in", req.url));
     }
   }
   return NextResponse.next();
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
